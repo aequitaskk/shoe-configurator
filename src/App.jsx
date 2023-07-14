7dc66ada4dc39b10739a023b9fd6f61225521d7f
dalmedian27 <kkla1124@gmail.com>
2023-07-14 18:51:46 -0400

    add modal with motion

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,20 @@ import {
 } from "@react-three/drei";
 import { HexColorPicker } from "react-colorful";
 import { proxy, useSnapshot } from "valtio";
+import Menu from "./components/Menu";
+
 
 const state = proxy({
   current: null,
   items: {
     laces: "#ffffff",
-    mesh: "#ffffff",
+    mesh: "#D1343A",
     caps: "#ffffff",
-    inner: "#ffffff",
+    inner: "#821717",
     sole: "#ffffff",
     stripes: "#ffffff",
     band: "#ffffff",
-    patch: "#ffffff",
+    patch: "#1a1a1a",
   },
 });
 
@@ -28,16 +30,6 @@ function Shoe() {
   const snap = useSnapshot(state);
   const { nodes, materials } = useGLTF("shoe-draco.glb");
   const [hovered, set] = useState(null);
-
-  useFrame((state) => {
-    const t = state.clock.getElapsedTime();
-    ref.current.rotation.set(
-      Math.cos(t / 4) / 8,
-      Math.sin(t / 4) / 8,
-      -0.2 - (1 + Math.sin(t / 1.5)) / 20
-    );
-    ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10;
-  });
 
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
@@ -62,6 +54,7 @@ function Shoe() {
       onClick={(e) => (
         e.stopPropagation(), (state.current = e.object.material.name)
       )}
+      rotation={[0, 0, -0.2]}
     >
       <mesh
         receiveShadow
@@ -126,23 +119,21 @@ function Shoe() {
 function Picker() {
   const snap = useSnapshot(state);
   return (
-    <div
-      style={{ display: snap.current ? "block" : "none" }}
-      className="absolute top-0 left-0"
-    >
-      <HexColorPicker
-        className="picker"
-        color={snap.items[snap.current]}
-        onChange={(color) => (state.items[snap.current] = color)}
-      />
-      <h1>{snap.current}</h1>
+    <div className="absolute top-10 left-40">
+      <div style={{ display: snap.current ? "block" : "none" }} className="space-y-1">
+        <h1 className="capitalize font-semibold text-3xl font-poppins">{snap.current} Color</h1>
+        <HexColorPicker
+          color={snap.items[snap.current]}
+          onChange={(color) => (state.items[snap.current] = color)}
+        />
+      </div>
     </div>
   );
 }
 
 export default function App() {
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative w-full h-screen bg-gradient-to-b from-violet-50 to-pink-50">
       <Canvas shadows camera={{ position: [0, 0, 4], fov: 45 }}>
         <ambientLight intensity={0.7} />
         <spotLight
@@ -156,18 +147,14 @@ export default function App() {
         <Environment preset="city" />
         <ContactShadows
           position={[0, -0.8, 0]}
-          opacity={0.25}
+          opacity={0.4}
           scale={10}
           blur={1.5}
           far={0.8}
         />
-        <OrbitControls
-          minPolarAngle={Math.PI / 2}
-          maxPolarAngle={Math.PI / 2}
-          enableZoom={false}
-          enablePan={false}
-        />
+        <OrbitControls />
       </Canvas>
+      <Menu />
       <Picker />
     </div>
   );
---
+++ src/components/Menu.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { IoSparklesSharp } from "react-icons/io5";
+import { IoMdDownload } from "react-icons/io";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Menu = () => {
+  const [showModal, setShowModal] = useState(false);
+  console.log(showModal);
+
+  return (
+    <div className="z-10 fixed bottom-10 left-[35%] lg:left-[45%]">
+      <div className="flex text-4xl space-x-6 border-2 py-2 px-6 bg-white/50 items-center cursor-pointer">
+        <IoSparklesSharp
+          className="hover:opacity-60 transition-opacity duration-150 ease-in-out"
+          onClick={() => setShowModal(!showModal)}
+        />
+        <IoMdDownload className="transform translate-y-1 hover:opacity-60 transition-opacity duration-150 ease-in-out" />
+      </div>
+      <AnimatePresence>
+        {showModal && (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.2 }}
+          >
+            <div className="absolute bottom-20 w-80 h-32 -translate-x-[92px] bg-white/50 backdrop-filter backdrop-blur-sm rounded-lg shadow-lg cursor-default px-4">
+              <p className="font-poppins font-semibold text-neutral-800 py-1">
+                Ask AI for design
+              </p>
+              <textarea
+                name="textarea"
+                cols="30"
+                rows="3"
+                className="opacity-60 outline-none mb-4 w-full px-2"
+              />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Menu;
