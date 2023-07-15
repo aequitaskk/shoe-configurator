c49dcd7f0635115af16948353481b353a19a8911
dalmedian27 <kkla1124@gmail.com>
2023-07-15 16:15:04 -0400

    generate ai image from input

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import { IoSparklesSharp } from "react-icons/io5";
 import { IoMdDownload } from "react-icons/io";
 import { motion, AnimatePresence } from "framer-motion";
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: import.meta.env.VITE_OPENAI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 const Menu = ({ canvasRef }) => {
   const [showModal, setShowModal] = useState(false);
@@ -18,6 +25,20 @@ const Menu = ({ canvasRef }) => {
       link.click();
     }
   };
+
+
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      prompt: input,
+      n: 1,
+      size: "1024x1024",
+    });
+    console.log(response);
+
+    const generatedImageUrl = response.data.data[0].url;
+    console.log(generatedImageUrl);
+  };
+
 
   return (
     <div className="z-10 fixed bottom-10 left-[35%] lg:left-[45%]">
@@ -51,7 +72,7 @@ const Menu = ({ canvasRef }) => {
                 rows="3"
                 className="opacity-60 outline-none mb-1 w-full px-2"
               />
-              <button className="text-white bg-slate-800 rounded-md py-1 px-2 font-medium flex mx-auto">
+              <button onClick={generateImage} className="text-white bg-slate-800 rounded-md py-1 px-2 font-medium flex mx-auto">
                 Generate
               </button>
             </div>
